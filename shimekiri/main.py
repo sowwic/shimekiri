dc93cc5e0aee103cb974242e36adbacec1c103a6
S0nic014 <loki.di90@gmail.com>
2020-10-06 19:24:48 +0100

    Fix fileFn create_file func, add exception catcher

--- shimekiri/main.py
+++ shimekiri/main.py
@@ -21,20 +21,23 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     # Setup logging, load config
     Logger.write_to_rotating_file("shimekiri.log")
-    app.setStyleSheet(load_style())
+    try:
+        app.setStyleSheet(load_style())
 
-    # Create tray
-    tray_widget = QtWidgets.QWidget()
-    tray_icon = tray.TrayIcon(QtGui.QIcon("./res/images/icons/death.png"), tray_widget)
-    tray_icon.show()
+        # Create tray
+        tray_widget = QtWidgets.QWidget()
+        tray_icon = tray.TrayIcon(QtGui.QIcon("./res/images/icons/death.png"), tray_widget)
+        tray_icon.show()
 
-    # Create dialog and show
-    main_dialog = WatcherDialog()
-    main_dialog.show()
-    Logger.set_level(Config.get("logging.level", default=10))
+        # Create dialog and show
+        main_dialog = WatcherDialog()
+        main_dialog.show()
+        Logger.set_level(Config.get("logging.level", default=10))
 
-    # Connect dialog to tray
-    tray_icon.open_deadliner_action.triggered.connect(main_dialog.show)
-    tray_icon.quit_app_action.triggered.connect(sys.exit)
+        # Connect dialog to tray
+        tray_icon.open_deadliner_action.triggered.connect(main_dialog.show)
+        tray_icon.quit_app_action.triggered.connect(sys.exit)
+    except Exception:
+        Logger.exception("Failed to run program")
 
     sys.exit(app.exec_())
